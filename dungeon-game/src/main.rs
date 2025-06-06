d7871a9955b0a76873bbac0926a6fa264c0932da
legoloaf17 <nguyenhoangha100209@gmail.com>
2025-06-06 21:22:34 +0700

    initialized dungeon-game

---
+++ dungeon-game/src/main.rs
@@ -0,0 +1,20 @@
+struct Solution;
+
+impl Solution {
+    fn recur() {
+        
+    }
+
+    pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
+        
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::calculate_minimum_hp(vec![
+        vec![-2,-3,3],
+        vec![-5,-10,1],
+        vec![10,30,-5]
+    ]), 7);
+    assert_eq!(Solution::calculate_minimum_hp(vec![vec![0]]), 1);
+}
