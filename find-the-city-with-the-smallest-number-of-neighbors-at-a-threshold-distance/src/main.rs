1300f0ec92ce884cfdb7b37f27e7bb4c4cb5bd01
legoloaf17 <nguyenhoangha100209@gmail.com>
2025-06-06 16:15:48 +0700

    completed find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance

--- find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/src/main.rs
+++ find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/src/main.rs
@@ -11,25 +11,24 @@ impl Solution {
         v
     }
 
-    fn dijkstra(n: i32, dt: i32, start: i32, graph: &Vec<Vec<(i32, i32)>>) -> Vec<i32> {
+    fn dijkstra(n: i32, start: i32, graph: &Vec<Vec<(i32, i32)>>) -> Vec<i32> {
         let mut set = std::collections::BTreeSet::new();
-        let mut distance = vec![-1; n as usize];
+        let mut distance = vec![i32::MAX; n as usize];
 
         distance[start as usize] = 0;
-        set.insert((start, 0));
+        set.insert((0, start));
 
         while !set.is_empty() {
             let tp = set.first().unwrap().clone();
             set.pop_first();
 
-            for v in &graph[tp.0 as usize] {
-                if distance[v.0 as usize] < distance[tp.0 as usize] + v.1 && distance[tp.0 as usize] + v.1 <= dt {
-                    set.remove(&(v.0, distance[v.0 as usize]));
-                    distance[v.0 as usize] = distance[tp.0 as usize] + v.1;
-                    set.insert((v.0, distance[v.0 as usize]));
+            for v in &graph[tp.1 as usize] {
+                if distance[v.0 as usize] > distance[tp.1 as usize] + v.1 {
+                    set.remove(&(distance[v.0 as usize], v.0));
+                    distance[v.0 as usize] = distance[tp.1 as usize] + v.1;
+                    set.insert((distance[v.0 as usize], v.0));
                 }
             }
-            
         }
         distance
     }
@@ -40,7 +39,7 @@ impl Solution {
         let mut hold_temp;
 
         for i in 0..n {
-            hold_temp = Self::dijkstra(n, distance_threshold, i, &v).iter().filter(|&x| *x != -1).count() as i32;
+            hold_temp = Self::dijkstra(n, i, &v).iter().filter(|&x| *x != i32::MAX && *x <= distance_threshold).count() as i32;
             if tp >= hold_temp { tp = hold_temp; res = i; }
         }
         res
@@ -48,5 +47,6 @@ impl Solution {
 }
 
 fn main() {
-    
+    assert_eq!(Solution::find_the_city(4, vec![ vec![0,1,3], vec![1,2,1], vec![1,3,4], vec![2,3,1]], 4), 3);
+    assert_eq!(Solution::find_the_city(5, vec![ vec![0,1,2], vec![0,4,8], vec![1,2,3], vec![1,4,2], vec![2,3,1], vec![3,4,1] ], 2), 0);
 }
