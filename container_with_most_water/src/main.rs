ec6a3788f5efbdb84aa49b7465cc37a4b842ed1f
legoloaf17 <nguyenhoangha100209@gmail.com>
2025-06-09 11:22:55 +0700

    solved container-with-most-water and add-two-numbers

--- container_with_most_water/src/main.rs
+++ container_with_most_water/src/main.rs
@@ -2,53 +2,25 @@ struct Solution;
 
 impl Solution {
     fn max_area(height: Vec<i32>) -> i32 {
-        let maxn = height.iter().max();
-        let mut second_maxn = height.iter().filter(|x| x < &maxn.unwrap()).max();
+        let n = height.len();
+        let (mut i, mut j) = (0, n-1);
+        let mut res = 0;
 
-        if second_maxn.is_none() { second_maxn = maxn; }
-
-        let mut m = Vec::new();
-        let mut n = Vec::new();
-
-        for i in 0..height.len() {
-            if height[i as usize] == *maxn.unwrap() { m.push(i); }
-            if height[i as usize] == *second_maxn.unwrap() { n.push(i); }
+        while i < j {
+            if height[i] < height[j] {
+                res = std::cmp::max(res, height[i] * (j-i) as i32);
+                i += 1;
+            } else {
+                res = std::cmp::max(res, height[j] * (j-i) as i32);
+                j -= 1;
+            }
         }
 
-        /* let (a,b) = (
-            (*n.last().unwrap() as i32 - *m.first().unwrap() as i32).abs(),
-            (*m.last().unwrap() as i32 - *n.first().unwrap() as i32).abs()
-        );
-
-        println!("{}, {}", a,b); */
-
-        let a = std::cmp::max(
-                (*n.last().unwrap() as i32 - *m.first().unwrap() as i32).abs(),
-                (*m.last().unwrap() as i32 - *n.first().unwrap() as i32).abs()
-        ) * second_maxn.unwrap();
-        let b = (m.last().unwrap() - m.first().unwrap()) as i32 * maxn.unwrap();
-        let c = (n.last().unwrap() - n.first().unwrap()) as i32 * second_maxn.unwrap();
-
-        // println!("{:?}, {:?}, {}, {}, {}", m.last(), m.first(), a,b,c);
-        std::cmp::max(a.max(b),c)
+        res
     }
 }
 
-// optimize for exercise?
-//
-// how to?
-//
-// height = min(h[h1], h[h2])
-// length = abs(h1 - h2)
-// max of height * length?
-//
-// height = h[h1]
-// => area = h2*h1 - h1^2
-// height = h2
-// => area = h1*h2 - h2^2
-
 fn main() {
-    println!("{}", Solution::max_area(vec![1,8,6,2,5,4,8,3,7]));
-    println!("{}", Solution::max_area(vec![1,1]));
-    println!("{}", Solution::max_area(vec![1,2,1]));
+    assert_eq!(49, Solution::max_area(vec![1,8,6,2,5,4,8,3,7]));
+    assert_eq!(1, Solution::max_area(vec![1,1]));
 }
